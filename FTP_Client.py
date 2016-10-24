39ce8df8cf01c5355efed0cd9ccf4c35432659f6
kartuhitam <destiana14@mhs.if.its.ac.id>
2016-10-24 17:06:53 +0700

    FTP Client 0.1.2

--- FTP_Client.py
+++ FTP_Client.py
@@ -6,27 +6,43 @@ ftp_port = 21
 def_timeout = _GLOBAL_DEFAULT_TIMEOUT
 break_line = '\r\n'
 max_line = 8192
-
-sys.stdout.write('>>> ')
-server_address = sys.stdin.readline()
-
-# create connection
-ftp_socket = socket.create_connection((server_address, ftp_port), def_timeout)
-ftp_family = ftp_socket.family
-ftp_welcome = ftp_socket.recv(max_line)
-sys.stdout.write(ftp_welcome)
+ftp_reply = ""
+ftp_socket = None
 
 # login to ftp server
-sys.stdout.write('>>> ')
-ftp_user = sys.stdin.readline()
-ftp_message = 'USER ' + ftp_user + break_line
-ftp_socket.send(ftp_message)
-ftp_reply = ftp_socket.recv(max_line)
-sys.stdout.write(ftp_reply)
-
-if ftp_reply[1:3] == 331:
+def login_ftp():
     sys.stdout.write('>>> ')
-    ftp_message = 'PASS ' + ftp_user + break_line
+    ftp_input = sys.stdin.readline()
+
+    ftp_message = 'USER ' + ftp_input + break_line
     ftp_socket.send(ftp_message)
-    ftp_reply = ftp_socket.recv(max_line)
-    sys.stdout.write(ftp_reply)
+    ftp_login_reply = ftp_socket.recv(max_line)
+    sys.stdout.write(ftp_login_reply)
+
+    sys.stdout.write('>>> ')
+    ftp_input = sys.stdin.readline()
+    ftp_message = 'PASS ' + ftp_input + break_line
+    ftp_socket.send(ftp_message)
+    ftp_login_reply = ftp_socket.recv(max_line)
+    sys.stdout.write(ftp_login_reply)
+    return ftp_login_reply
+
+# create connection
+def create_connection():
+    sys.stdout.write('>>> ')
+    server_address = sys.stdin.readline()
+    ftp_new_socket = socket.create_connection((server_address, ftp_port), def_timeout)
+    ftp_welcome = ftp_new_socket.recv(max_line)
+    sys.stdout.write(ftp_welcome)
+    return ftp_new_socket
+
+# start of program
+try:
+    ftp_socket = create_connection()
+    ftp_reply = login_ftp()
+
+    while ftp_reply[0:3] == '530':
+        ftp_reply = login_ftp()
+
+except:
+    sys.exit()
