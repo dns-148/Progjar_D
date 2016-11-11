0915720404666a8d888d3fa856cb0919c1c91003
kartuhitam <destiana14@mhs.if.its.ac.id>
2016-11-11 13:43:30 +0700

    HTML SC 0.1.1

---
+++ HTML 1 - 6/soal 1.py
@@ -0,0 +1,15 @@
+import socket
+import sys
+
+max_line = 8192
+server_address = ('quizajk.if.its.ac.id', 80)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(server_address)
+
+req_message = "GET / HTTP/1.1\r\nHost: quizajk.if.its.ac.id\r\n\r\n"
+client_socket.send(req_message)
+response = client_socket.recv(max_line)
+str_response = str(response).splitlines()
+output = str_response[0] + "\n"
+sys.stdout.write(output)
+client_socket.close()
