83036eeecf623b644d488cac82b07d285774910c
kartuhitam <destiana14@mhs.if.its.ac.id>
2016-11-12 07:28:56 +0700

    HTML SC 0.1.5

---
+++ HTML 1 - 6/soal 5.py
@@ -0,0 +1,24 @@
+import socket
+# import sys
+from bs4 import BeautifulSoup
+
+max_size = 8192
+response = ""
+server_address = ('elearning.if.its.ac.id', 80)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(server_address)
+
+req_message = "GET / HTTP/1.1\r\nHost: elearning.if.its.ac.id\r\n\r\n"
+client_socket.send(req_message)
+
+while True:
+    resp_parts = client_socket.recv(max_size)
+    response = response + resp_parts
+    if "</html>" in resp_parts:
+        break
+
+html_resp = response.partition("<!DOCTYPE html>")[1] + response.partition("<!DOCTYPE html>")[2]
+soup = BeautifulSoup(response, "html.parser")
+for node in soup.find_all("div", class_="subject"):
+    print node.text
+client_socket.close()
